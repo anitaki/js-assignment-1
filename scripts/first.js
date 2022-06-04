2432387f5c8c4b799bb11c4ec65a99e66c2b38d1
Anita Angelidou <anitawebdesigns@gmail.com>
2022-06-04 17:22:33 +0300

    stage 2

---
+++ scripts/first.js
@@ -0,0 +1,36 @@
+//  Assignment 2
+
+// Write a JavaScript program to find:
+
+//1. the diagonal of a square where the length of each side is 9.
+
+let diagonal = 9 * Math.SQRT2;
+console.log(diagonal);
+
+// 2. the area of a triangle where lengths of the three of its sides are 5, 6 and 7.
+
+let s = (5 + 6 + 7) / 2;
+let heron = Math.sqrt(s * (s - 5) * (s - 6) * (s - 7));
+console.log(heron);
+
+//3. the circumference and surface area of a circle whose radius is 4.
+
+const p = 3.1415926535897932384626433;
+let circumference = 2 * p * 4;
+console.log(circumference);
+let surfaceArea = p * 4 ** 2;
+console.log(surfaceArea);
+
+// Assignment 3
+
+// 1. Write a JavaScript program that accepts two integers and displays the larger of the two.
+
+let comparison = (a, b) => a > b ? console.log(a) : console.log(b);
+comparison(5, 12);
+comparison(35, 758);
+
+// 2. Write a JavaScript program that checks whether an integer is an even or an odd number.
+
+let checker = (a) => a % 2 == 0 ? console.log(a + " is an even number") : console.log(a + " is an odd number");
+checker(5);
+checker(8);
