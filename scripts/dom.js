96a5ca8d94a0b8bcbe21512d63e793d9cb2f5269
Anita Angelidou <anitawebdesigns@gmail.com>
2022-06-08 17:40:48 +0300

    replace const p with math.pi

---
+++ scripts/dom.js
@@ -0,0 +1,5 @@
+
+const p1=document.createElement("p")
+document.getElementsByTagName("p").innerHTML="HI"
+
+
--- scripts/functions.js
+++ scripts/functions.js
@@ -9,38 +9,40 @@ const items = ['rock', 'paper', 'scissors']
     return item;    
 }
 
-// const computerSelection = computerPlay();
 
-
-// let player = prompt("What is your move? Rock, Paper or Scissors?");
-// const playerSelection = player.toLowerCase();
 
 let result;
 
 function playRound(playerSelection, computerSelection) {
     
     if (playerSelection =="rock" && computerSelection == "paper") {
-        return result = "You lose! Paper beats Rock"
+        console.log("You lose! Paper beats Rock")
+        return result = -1
     }
     else if (playerSelection == "paper" && computerSelection == "rock") {
-        return result = "You win! Paper beats Rock"
+        console.log("You win! Paper beats Rock")
+        return result = 1
     }
     else if (playerSelection == "rock" && computerSelection == "scissors") {
-        return result = "You win! Rock beats Scissors"
+        console.log("You win! Rock beats Scissors")
+        return result = 1
     }
     else if (playerSelection == "scissors" && computerSelection == "rock") {
-        return result = "You lose! Rock beats Scissors"
+        console.log("You lose! Rock beats Scissors")
+        return result = -1
     }
     else if (playerSelection == "paper" && computerSelection == "scissors") {
-        return result = "You lose! Scissors beats Paper"
+        console.log("You lose! Scissors beats Paper")
+        return result = -1
     }
     else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return result = "You win! Scissors beats Paper"
+        console.log("You win! Scissors beats Paper")
+        return result = 1
     }
-
     else if (playerSelection === computerSelection) {
         return result = "It's a tie!"
     }
+
     else {
         return result = "Not a valid selection"
     }
@@ -60,47 +62,3 @@ function game() {
     }
 }
 game();
-
-
-
-
-
-
-
-
-
-// switch (playerSelection, computerSelection) {
-//     case 0:
-//         playerSelection = "Rock"
-//         computerSelection = "Paper"
-//        return "You lose! Paper beats Rock"
-//         break;
-//     case 1:
-//         playerSelection = "Paper"
-//         computerSelection = "Rock"
-//         return "You win! Paper beats Rock"
-//         break;
-//     case 2:
-//         playerSelection = "Rock"
-//         computerSelection = "Scissors"
-
-//         return ("You win! Rock beats Scissors")
-//         break;
-//     case 3:
-//         playerSelection = "Scissors"
-//         computerSelection = "Rock"
-
-//         return ("You lose! Rock beats Scissors")
-//         break;
-//     case 4:
-//         playerSelection = "Paper"
-//         computerSelection = "Scissors"
-
-//         return ("You lose! Scissors beats Paper")
-//         break;
-//     case 5:
-//         playerSelection = "Scissors"
-//         computerSelection = "Paper"
-//         return ("You win! Scissors beats Paper")
-// }
-
