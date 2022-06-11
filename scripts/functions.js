c5d83bd0deee876aeb4c5a115ed2997fef6e107b
Anita Angelidou <anitawebdesigns@gmail.com>
2022-06-11 16:12:04 +0300

    added colors to winnerRoundText

--- scripts/functions.js
+++ scripts/functions.js
@@ -2,97 +2,132 @@
 
 // Assignment 4
 
-const rock=document.querySelector("#rock").dataset.action
-const paper=document.querySelector("#paper").dataset.action
-const scissors=document.querySelector("#scissors").dataset.action
-const selection=document.querySelectorAll(".selection")
+const rock = document.querySelector("#rock").dataset.action
+const paper = document.querySelector("#paper").dataset.action
+const scissors = document.querySelector("#scissors").dataset.action
+const selection = document.querySelectorAll(".selection")
+
+let ppText = document.getElementById("player")
+let pcText = document.getElementById("computer")
+let pwText = document.getElementById("winner")
+let ppsText = document.getElementById("player-score")
+let pgoText = document.getElementById("game-over")
+
+let playerScore = 0
+let computerScore = 0
+
 
 const items = ['rock', 'paper', 'scissors']
 function computerPlay() {
     let item = items[Math.floor(Math.random() * items.length)];
-    return item;    
+    return item;
 }
 
 let playerRound;
-function playerPlay(number){
-      playerRound= items[number]
-      console.log(playerRound)
-      return playerRound      
+function playerPlay(number) {
+    playerRound = items[number]
+    console.log(playerRound)
+    return playerRound
 }
 
 let result;
-let winnerRound;
+let winnerRoundText;
+
 function playRound(playerSelection, computerSelection) {
-    
-    if (playerSelection =="rock" && computerSelection == "paper") {
-        winnerRound = "You lose! Paper beats Rock"
+
+   if (playerSelection == "rock" && computerSelection == "paper") {
+        winnerRoundText = `<span class="lose">You lose! Paper beats Rock</span>`
         return result = -1
     }
     else if (playerSelection == "paper" && computerSelection == "rock") {
-        winnerRound = "You win! Paper beats Rock"
+        winnerRoundText = `<span class="win">You win! Paper beats Rock</span>`
         return result = 1
     }
     else if (playerSelection == "rock" && computerSelection == "scissors") {
-        winnerRound = "You win! Rock beats Scissors"
+        winnerRoundText = `<span class="win">You win! Rock beats Scissors</span>`
         return result = 1
     }
     else if (playerSelection == "scissors" && computerSelection == "rock") {
-        winnerRound = "You lose! Rock beats Scissors"
+        winnerRoundText = `<span class="lose">You lose! Rock beats Scissors</span>`
         return result = -1
     }
     else if (playerSelection == "paper" && computerSelection == "scissors") {
-        winnerRound = "You lose! Scissors beats Paper"
+        winnerRoundText = `<span class="lose">You lose! Scissors beats Paper</span>`
         return result = -1
     }
     else if (playerSelection == "scissors" && computerSelection == "paper") {
-        winnerRound = "You win! Scissors beats Paper"
+        winnerRoundText = `<span class="win">You win! Scissors beats Paper</span>`
         return result = 1
     }
     else if (playerSelection === computerSelection) {
-        winnerRound = "It's a tie!"
+        winnerRoundText = `<span class="tie">It's a tie!</span>`
         return result = 0
     }
-
     else {
         return result = "Not a valid selection"
     }
 }
 
-let playerScore=0
-let computerScore=0
-let finalScore = playerScore+computerScore
-  
-function score(result){
- result === 1 ? playerScore+=1 : // if 
- result === -1 ? computerScore+=1 : // else if 
-null // else 
-return playerScore;
-return computerScore;
+function score(result) {
+    result === 1 ? playerScore += 1 : // if 
+        result === -1 ? computerScore += 1 : // else if 
+            null // else 
+
+    return [playerScore, computerScore]
 }
 
 
-
-function game(){
-    const computerSelection = computerPlay();
+function game() {
+   
     const playerSelection = playerRound
-    let ppText = document.getElementById("player")
-    ppText.innerHTML = "Player: "+ playerSelection
-    let pcText = document.getElementById("computer")
-    pcText.innerHTML = "Computer: "+ computerSelection
+    const computerSelection = computerPlay();
+
+    ppText.innerHTML = "Player: " + playerSelection
+    pcText.innerHTML = "Computer: " + computerSelection
     
     playRound(playerSelection, computerSelection)
     score(result)
 
-    let pwText = document.getElementById("winner")
-    pwText.innerHTML = winnerRound
-    let ppsText = document.getElementById("player-score")
-    ppsText.innerHTML = "Your score is " + playerScore + " and the computer's score is " + computerScore
+pwText.innerHTML = winnerRoundText
 
-    console.log(winnerRound);
-    console.log(playerScore, computerScore)
+
+ppsText.innerHTML = "Your score is " + playerScore + " and the computer's score is " + computerScore
+
+    function endGame(playerScore, computerScore) {
+        if (playerScore === 5) {
+            pgoText.innerHTML = "YOU WIN!"
+        }
+        else if (computerScore === 5) {
+            pgoText.innerHTML = "GAME OVER! YOU LOSE"
+        }
+        else if (playerScore>5||computerScore>5){
+            newGame()
+        }
+        else {
+        }
+    }
+
+    endGame(playerScore, computerScore)
+   
 }
 
-game();
+function newGame() {
+    winnerRoundText = ""
+    ppText.innerHTML="Player:"
+    pcText.innerHTML="Computer:"
+    pwText.innerHTML=""
+    ppsText.innerHTML = ""
+    pgoText.innerHTML=""
+    playerScore=0
+    computerScore=0
+    result=0 
+    return
+}
+
+
+
+
+
 
 
 
